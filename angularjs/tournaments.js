c608656efca753baa5fba7697fba315d78bf92ed
EPICmynamesBG <mynamesbg@gmail.com>
2015-08-23 22:30:17 -0400

    Fall updates

--- angularjs/tournaments.js
+++ angularjs/tournaments.js
@@ -1,101 +1,129 @@
-function TournamentsController($http,$scope,$route, $location) {
+function TournamentsController($http, $scope, $route, $location) {
     $scope.events = [{
-        title: "Taylor",
-        dateStart: "Mar 21",
-        dateEnd : "Mar 21",
-        location: "Upland, IN",
-        description : "A quick bid, 1 day tournament at Taylor University, this was a warm up tournament for Nashville. Coming out hot and heavy, Ball U Men in help with some of the girls team and other friends swept the tournament 3-0, taking home the grand prize of: nothing! But this tournament was a blast for everyone, and the ice cream afterwards was the cherry on the pie!",
-        images : [
-            "images/tournaments/Taylor1.jpg"
+        title: "Huckz for Dayz",
+        dateStart: "Sept 12",
+        dateEnd: "Sept 13",
+        location: "Indiana Wesleyan University, IN",
+        description: "The first scheduled tournament of the year, this medium sized tournament will give rookies their first chance to strut their stuff. But don't get discouraged, because there are plenty of tournaments remaining and time to improve.",
+        images: [
         ]
     }, {
-        title: "Music City",
-        dateStart: "Mar 28",
-        dateEnd : "Mar 29",
-        location: "Nashville, TN",
-        description : "Located in Nashville, TN, Music City is massive tournament with many big name teams like UT and Alabama at it. This was also our first sanctioned tournament of the year. Overall, the team went 1-6, but it was an enlightening experience that the team will surely learn from!",
-        images : [
-            "images/tournaments/MusicCity1.jpg"
-        ]
-    },{
-        title: "Decatur",
-        dateStart: "April 4",
-        dateEnd : "April 4",
-        location: "Decatur, IN",
-        description : "Located in Decatur, IN, this is a small tournament about 1.5hrs from Ball State. With a team of 12 that including both current and Alumni players, the team took first in a 5-0 sweep. While we may have won the tournament, there were many points in the day that showed there is still room for improvement before tournament!",
-        images : [
-            "images/tournaments/Decatur1.jpg"
+        title: "Boiler Breakaway",
+        dateStart: "Sept 19",
+        dateEnd: "Sept 20",
+        location: "Purdue University, IN",
+        description: "Hosted at the grand ol' Purdue University, this massive tournament features a number of high caliber teams, and will prove to be a great test for all team members, new and old.",
+        images: [
         ]
     }, {
-        title: "Conference Tournament",
-        dateStart: "Apr 11",
-        dateEnd : "Apr 12",
-        location : "Evansville, IN",
-        description : "Located in Evansville, IN, this was the most important tournament of the season. Last year, the team didn't make such a great appearance, but that all changed this year. This year, the team went 3-4 on the weekend, and even gave teams like Notre Dame a run for their money. The team showed a lot of ferocity over the weekend, and although we will miss those who will be leaving us after this semester, at least they can be proud of how this young team performed. ",
-        images : [
-            "images/tournaments/Conference16.jpg",
-            "images/tournaments/Conference1.jpg",
-            "images/tournaments/Conference2.jpg",
-            "images/tournaments/Conference3.jpg",
-            "images/tournaments/Conference4.jpg",
-            "images/tournaments/Conference5.jpg",
-            "images/tournaments/Conference6.jpg",
-            "images/tournaments/Conference7.jpg",
-            "images/tournaments/Conference8.jpg",
-            "images/tournaments/Conference9.jpg",
-            "images/tournaments/Conference10.jpg",
-            "images/tournaments/Conference11.jpg",
-            "images/tournaments/Conference12.jpg",
-            "images/tournaments/Conference12_2.jpg",
-            "images/tournaments/Conference13.jpg",
-            "images/tournaments/Conference14.jpg",
-            "images/tournaments/Conference15.jpg",
-            "images/tournaments/Conference18.jpg",
-            "images/tournaments/Conference19.jpg",
-            "images/tournaments/Conference20.jpg",
-            "images/tournaments/Conference21.jpg",
-            "images/tournaments/Conference22.jpg",
-            "images/tournaments/Conference23.jpg",
-            "images/tournaments/Conference24.jpg",
-            "images/tournaments/Conference25.jpg",
-            "images/tournaments/Conference26.jpg",
-            "images/tournaments/Conference27.jpg",
-            "images/tournaments/Conference28.jpg",
-            "images/tournaments/Conference29.jpg",
-            "images/tournaments/Conference30.jpg",
-            "images/tournaments/Conference31.jpg",
-            "images/tournaments/Conference32.jpg",
-            "images/tournaments/Conference33.jpg",
-            "images/tournaments/Conference34.jpg",
-            "images/tournaments/Conference35.jpg",
-            "images/tournaments/Conference36.jpg",
-            "images/tournaments/Conference37.jpg",
-            "images/tournaments/Conference38.jpg",
-            "images/tournaments/Conference39.jpg",
-            "images/tournaments/Conference40.jpg",
-            "images/tournaments/Conference41.jpg",
-            "images/tournaments/Conference42.jpg",
-            "images/tournaments/Conference43.jpg",
-            "images/tournaments/Conference44.jpg",
-            "images/tournaments/Conference45.jpg",
-            "images/tournaments/Conference46.jpg",
-            "images/tournaments/Conference47.jpg",
-            "images/tournaments/Conference48.jpg",
-            "images/tournaments/Conference49.jpg",
-            "images/tournaments/Conference50.jpg",
-            "images/tournaments/Conference51.jpg",
-            "images/tournaments/Conference52.jpg"
-        ]
-    }, {
-        title: "Poultry Days",
-        dateStart: "June 13",
-        dateEnd : "June 14",
-        location: "Versailles, OH",
-        description : "Located in small town Versailles, OH, this is one of the largest and oldest tournaments in the US. The event is famous for bringing in high calibur international teams like Team USA, Canada, and even nations like the UK, China, Austraillia, and more. The Poultry Days festival actually goes starts on June 12 (Friday) and is only a 5 minute walk from the Ultimate Fields. The festival is renowned for its one of a kind BBQ chicken. This event is not a BSU team tournament, but many summer club teams in the area are going and add-on members are generally welcome to jump in. While most players choose to pitch a tent at the Ultimate fields, Brandon Groff does live in Versailles and housing can be arranged. More information can be found at www.poultrydays.com",
-        images : [
-            "images/tournaments/PDays1.jpg",
-            "images/tournaments/PDays2.jpg",
-            "images/tournaments/PDays3.gif"
+        title: "States Tournament",
+        dateStart: "Sept 27",
+        dateEnd: "Sept 27",
+        location: "Ball State University, IN",
+        description: "BSU's own tournament, a one-day smedium sized tournament featuring a number of teams ranging in skills, giving teams a chance to both test their skills and learn from better teams.",
+        images: [
         ]
     }];
+
+    /**
+        $scope.Spring2015 = [{
+            title: "Taylor",
+            dateStart: "Mar 21",
+            dateEnd: "Mar 21",
+            location: "Upland, IN",
+            description: "A quick bid, 1 day tournament at Taylor University, this was a warm up tournament for Nashville. Coming out hot and heavy, Ball U Men in help with some of the girls team and other friends swept the tournament 3-0, taking home the grand prize of: nothing! But this tournament was a blast for everyone, and the ice cream afterwards was the cherry on the pie!",
+            images: [
+                "images/tournaments/Taylor1.jpg"
+            ]
+        }, {
+            title: "Music City",
+            dateStart: "Mar 28",
+            dateEnd: "Mar 29",
+            location: "Nashville, TN",
+            description: "Located in Nashville, TN, Music City is massive tournament with many big name teams like UT and Alabama at it. This was also our first sanctioned tournament of the year. Overall, the team went 1-6, but it was an enlightening experience that the team will surely learn from!",
+            images: [
+                "images/tournaments/MusicCity1.jpg"
+            ]
+        }, {
+            title: "Decatur",
+            dateStart: "April 4",
+            dateEnd: "April 4",
+            location: "Decatur, IN",
+            description: "Located in Decatur, IN, this is a small tournament about 1.5hrs from Ball State. With a team of 12 that including both current and Alumni players, the team took first in a 5-0 sweep. While we may have won the tournament, there were many points in the day that showed there is still room for improvement before tournament!",
+            images: [
+                "images/tournaments/Decatur1.jpg"
+            ]
+        }, {
+            title: "Conference Tournament",
+            dateStart: "Apr 11",
+            dateEnd: "Apr 12",
+            location: "Evansville, IN",
+            description: "Located in Evansville, IN, this was the most important tournament of the season. Last year, the team didn't make such a great appearance, but that all changed this year. This year, the team went 3-4 on the weekend, and even gave teams like Notre Dame a run for their money. The team showed a lot of ferocity over the weekend, and although we will miss those who will be leaving us after this semester, at least they can be proud of how this young team performed. ",
+            images: [
+                "images/tournaments/Conference16.jpg",
+                "images/tournaments/Conference1.jpg",
+                "images/tournaments/Conference2.jpg",
+                "images/tournaments/Conference3.jpg",
+                "images/tournaments/Conference4.jpg",
+                "images/tournaments/Conference5.jpg",
+                "images/tournaments/Conference6.jpg",
+                "images/tournaments/Conference7.jpg",
+                "images/tournaments/Conference8.jpg",
+                "images/tournaments/Conference9.jpg",
+                "images/tournaments/Conference10.jpg",
+                "images/tournaments/Conference11.jpg",
+                "images/tournaments/Conference12.jpg",
+                "images/tournaments/Conference12_2.jpg",
+                "images/tournaments/Conference13.jpg",
+                "images/tournaments/Conference14.jpg",
+                "images/tournaments/Conference15.jpg",
+                "images/tournaments/Conference18.jpg",
+                "images/tournaments/Conference19.jpg",
+                "images/tournaments/Conference20.jpg",
+                "images/tournaments/Conference21.jpg",
+                "images/tournaments/Conference22.jpg",
+                "images/tournaments/Conference23.jpg",
+                "images/tournaments/Conference24.jpg",
+                "images/tournaments/Conference25.jpg",
+                "images/tournaments/Conference26.jpg",
+                "images/tournaments/Conference27.jpg",
+                "images/tournaments/Conference28.jpg",
+                "images/tournaments/Conference29.jpg",
+                "images/tournaments/Conference30.jpg",
+                "images/tournaments/Conference31.jpg",
+                "images/tournaments/Conference32.jpg",
+                "images/tournaments/Conference33.jpg",
+                "images/tournaments/Conference34.jpg",
+                "images/tournaments/Conference35.jpg",
+                "images/tournaments/Conference36.jpg",
+                "images/tournaments/Conference37.jpg",
+                "images/tournaments/Conference38.jpg",
+                "images/tournaments/Conference39.jpg",
+                "images/tournaments/Conference40.jpg",
+                "images/tournaments/Conference41.jpg",
+                "images/tournaments/Conference42.jpg",
+                "images/tournaments/Conference43.jpg",
+                "images/tournaments/Conference44.jpg",
+                "images/tournaments/Conference45.jpg",
+                "images/tournaments/Conference46.jpg",
+                "images/tournaments/Conference47.jpg",
+                "images/tournaments/Conference48.jpg",
+                "images/tournaments/Conference49.jpg",
+                "images/tournaments/Conference50.jpg",
+                "images/tournaments/Conference51.jpg",
+                "images/tournaments/Conference52.jpg"
+            ]
+        }, {
+            title: "Poultry Days",
+            dateStart: "June 13",
+            dateEnd: "June 14",
+            location: "Versailles, OH",
+            description: "Located in small town Versailles, OH, this is one of the largest and oldest tournaments in the US. The event is famous for bringing in high calibur international teams like Team USA, Canada, and even nations like the UK, China, Austraillia, and more. The Poultry Days festival actually goes starts on June 12 (Friday) and is only a 5 minute walk from the Ultimate Fields. The festival is renowned for its one of a kind BBQ chicken. This event is not a BSU team tournament, but many summer club teams in the area are going and add-on members are generally welcome to jump in. While most players choose to pitch a tent at the Ultimate fields, Brandon Groff does live in Versailles and housing can be arranged. More information can be found at www.poultrydays.com",
+            images: [
+                "images/tournaments/PDays1.jpg",
+                "images/tournaments/PDays2.jpg",
+                "images/tournaments/PDays3.gif"
+            ]
+        }];
+    **/
 }
